3cedc34aa89237d279fdbf35866d87842c48e9eb
matt <hwqla@foxmail.com>
2025-10-25 13:55:58 +0100

    Continuous action in a line(walk_away & a to b in x axis)

--- Bezier_trajectory/quadrupedal_walk_away.py
+++ Bezier_trajectory/quadrupedal_walk_away.py
@@ -10,21 +10,6 @@ import pinocchio
 import crocoddyl
 from crocoddyl.utils.quadruped import SimpleQuadrupedalGaitProblem, plotSolution
 
-def yaw_to_quat(yaw):
-    R = pinocchio.rpy.rpyToMatrix(0, 0, yaw)
-    q = pinocchio.Quaternion(R).coeffs()
-    return np.array(q)
-
-def set_base_xy_yaw(q, x, y, yaw):
-    q = q.copy()
-    quat = yaw_to_quat(yaw)
-    q[0:4] = quat
-    q[4:7] = np.array([x, y, q[6] if len(q) > 6 else 0.0])
-    return q
-
-def get_base_xy(q):
-    return float(q[0]), float(q[1])
-
 WITHDISPLAY = "display" in sys.argv or "CROCODDYL_DISPLAY" in os.environ
 WITHPLOT = "plot" in sys.argv or "CROCODDYL_PLOT" in os.environ
 signal.signal(signal.SIGINT, signal.SIG_DFL)
@@ -45,16 +30,6 @@ q0 = robot_model.referenceConfigurations["standing"].copy()
 v0 = pinocchio.utils.zero(robot_model.nv)
 x0 = np.concatenate([q0, v0])
 
-# Define the point A and B
-A = np.array([0.0, 0.0])
-B = np.array([1.0, 0.0])
-stop_tol = 0.05
-
-# dir_vec = B - A
-# yaw_des = float(np.arctan2(dir_vec[1], dir_vec[0]))
-# x0[:anymal.model.nq] = set_base_xy_yaw(x0[:anymal.model.nq], A[0], A[1], yaw_des)
-
-base_step_len = 0.20
 # Defining the walking gait parameters
 walking_gait = {
         "stepLength": 0.15,
@@ -65,12 +40,12 @@ walking_gait = {
 }
 
 # Setting up the control-limited DDP solver
-def build_walking_solver(x_int, step_len):
-    robot_model.referenceConfigurations["standing"] = x_int[:anymal.model.nq].copy()
+def build_walking_solver(x_int):
+
     solver = crocoddyl.SolverBoxDDP(
         gait.createWalkingProblem(
             x_int,
-            step_len,
+            walking_gait["stepLength"],
             walking_gait["stepHeight"],
             walking_gait["timeStep"],
             walking_gait["stepKnots"],
@@ -94,22 +69,23 @@ if WITHDISPLAY:
     display.freq = 1
 
 
+
+
 prev_xs, prev_us = None, None
 phase_idx = 0
 while True:
-    q_cur = x0[:anymal.model.nq]
-    x_cur, y_cur = get_base_xy(q_cur)
-    print("x_cur = ", x_cur)
-    rem = float(B[0] - x_cur)
-    # if x_cur > B[0]:
-    #     print("Reached B, Done")
-    #     break
-    if rem < stop_tol:
-        print(f"Reached B within {stop_tol} m, Done")
-        break
-    step_len = min(base_step_len, rem)
-    solver = build_walking_solver(x0, step_len)
-    print("rem = ", rem)
+    solver = build_walking_solver(x0)
+    # Add the callback functions
+    print("*** SOLVE ***")
+    if WITHPLOT:
+        solver.setCallbacks(
+            [
+                crocoddyl.CallbackVerbose(),
+                crocoddyl.CallbackLogger(),
+            ]
+        )
+    else:
+        solver.setCallbacks([crocoddyl.CallbackVerbose()])
 
     if prev_us is not None and prev_xs is not None:
         T = solver.problem.T
@@ -121,16 +97,7 @@ while True:
         xs0 = [x0] * (solver.problem.T + 1)
         us0 = solver.problem.quasiStatic([x0] * solver.problem.T)
 
-    print(f"*** SOLVED walking (phase {phase_idx}), rem={rem:.3f} m, step_len={step_len:.3f} m ***")
-    if WITHPLOT:
-        solver.setCallbacks(
-            [
-                crocoddyl.CallbackVerbose(),
-                crocoddyl.CallbackLogger(),
-            ]
-        )
-    else:
-        solver.setCallbacks([crocoddyl.CallbackVerbose()])
+    print(f"*** SOLVED walking (phase {phase_idx})***")
     solver.solve(xs0, us0, 100, False, 0.1)
 
     x0 = solver.xs[-1]
@@ -143,9 +110,25 @@ while True:
     # us = solver.problem.quasiStatic([x0] * solver.problem.T)
     # solver.solve(xs, us, 100, False, 0.1)
 
+    # Plotting the entire motion
+    if WITHPLOT:
+        # Plot control vs limits
+        plotSolution(solver, bounds=True, figIndex=1, show=False)
+
+        # Plot convergence
+        log = solver.getCallbacks()[1]
+        crocoddyl.plotConvergence(
+            log.costs,
+            log.pregs,
+            log.dregs,
+            log.grads,
+            log.stops,
+            log.steps,
+            figIndex=3,
+            show=True,
+        )
+
     if WITHDISPLAY and display is not None:
         display.displayFromSolver(solver)
 
     time.sleep(0.05)
-
-
