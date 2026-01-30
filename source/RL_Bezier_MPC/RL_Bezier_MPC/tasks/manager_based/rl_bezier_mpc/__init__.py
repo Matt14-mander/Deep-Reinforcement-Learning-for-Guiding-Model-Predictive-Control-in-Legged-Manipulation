684c2b40f4b6ce3895ac39586be92a73b1d0e1b2
matt <hwqla@foxmail.com>
2026-01-30 15:58:32 +0000

    Initial commit

---
+++ source/RL_Bezier_MPC/RL_Bezier_MPC/tasks/manager_based/rl_bezier_mpc/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+
+gym.register(
+    id="Template-Rl-Bezier-Mpc-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.rl_bezier_mpc_env_cfg:RlBezierMpcEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
+    },
+)
